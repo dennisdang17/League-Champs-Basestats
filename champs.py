795e710475d8acd561917bf2bd2b567698f5da03
dennisdang17 <dennisdang17@gmail.com>
2021-05-03 17:50:48 -0700

    Somewhere

--- champs.py
+++ champs.py
@@ -1,14 +1,25 @@
 from collections import OrderedDict
 from operator import itemgetter
 from pytest_bdd import scenarios, when, then
+from dotenv import load_dotenv
 from riotwatcher import LolWatcher
+
+import os
 import pytest
 
-lol_watcher = LolWatcher('RGAPI-ff1d3c7a-90c9-4e4b-954c-404ab6543f55')
-scenarios('../DeployingWorking/League-Champs-Basestats/league_champion.feature')
+load_dotenv()
+lol_watcher = LolWatcher(os.getenv('RIOT_TOKEN'))
+my_region = os.getenv('REGION')
+scenarios('../mnt/c/Users/denni/Documents/Code/DeployingWorking/League-Champs-Basestats/league_champion.feature')
 
 my_region = 'na1'
 
+class Response:
+    response_code = 0
+    sorted_list = {}
+    def __init__(self, sorted_list):
+        self.response_code = 200
+        self.sorted_list = sorted_list
 
 def get_champ_data_list():
     versions = lol_watcher.data_dragon.versions_for_region(my_region)
@@ -22,13 +33,15 @@ def get_champ_data_list():
 def best_champ_for_stat(base_stat):
     data_list = get_champ_data_list()
     sorted_list = OrderedDict()
-    print(data_list)
     for champ in data_list:
         sorted_list[champ] = data_list[champ]['stats'][base_stat]
     sorted_list = sorted(sorted_list.items(), key = lambda x: int(x[1]), reverse=True)
-    print(sorted_list[0])
-    return sorted_list[0]
-
-@then('the champion returned should be "<champion>"')
-def base_stat_returns_champion(sorted_list, champion):
-    assert sorted_list[0] == champion
+    response = Response(sorted_list)
+    print(response.sorted_list)
+    return response
+@then('the response status code is 200') 
+def status_code_check(stat_response):
+    assert stat_response.response_code == 200
+@then('the champion is "<champion>"')
+def base_stat_returns_champion(stat_response, champion):
+    assert stat_response.sorted_list[0][0] == champion
--- league_champion.feature
+++ league_champion.feature
@@ -3,11 +3,10 @@ Feature: League Champion Base State Sorted List
   I want to know the base stats of a league champion,
   So that I can know what is the strongest champion level 1 without items.
 
-
-  Scenario Outline: My API will be queried with a base_stat to return the champ with that highest base stat
-    When the League API is queried with "<base_stat>"
+  Scenario Outline: My API QUery
+    When my API is queried with "<base_stat>"
     Then the response status code is 200
-    And the response is the "<champion>"
+    And the champion is "<champion>"
 
     Examples: Champions
       | base_stat           | champion         |
